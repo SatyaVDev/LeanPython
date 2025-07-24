a9f6bb954abdd9fb44502ed824a524760f4fef28
Satish Vishwakarma <staya9125@gmail.com>
2025-07-25 00:01:16 +0530

    check empty foler

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ import os
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 files = utils.FileUtils(file_path=script_dir)
-print(files.get_all_files())
-print("total file count : ", files.get_file_count())
+print(files.get_empty_folder())
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -4,26 +4,31 @@ import os
 
 class FileUtils:
 
-  def __init__(self, file_path):
-    self.file_path = file_path
+    def __init__(self, file_path):
+        self.file_path = file_path
 
-  def get_all_files(self):
-    '''
-     Retrieve all files in the specified folder.
-     Returns:
-     list of str: A list containing the names of all files (not directories)
-                  present in the folder specified by folder_path.
-    '''
-    files = []
+    def get_all_files(self):
+        """
+        Retrieve all files in the specified folder.
+        Returns:
+        list of str: A list containing the names of all files (not directories)
+                     present in the folder specified by folder_path.
+        """
+        files = []
 
-    for item in os.listdir(self.file_path):
-      full_path = os.path.join(self.file_path, item)
-      if os.path.isfile(full_path):
-        files.append(item)
-    return files
+        for item in os.listdir(self.file_path):
+            full_path = os.path.join(self.file_path, item)
+            if os.path.isfile(full_path):
+                files.append(item)
+        return files
 
-  def get_file_count(self):
-    return len(self.get_all_files())
+    def get_file_count(self):
+        return len(self.get_all_files())
+
+    def get_empty_folder(self):
+
+        for root, folder, file in os.walk(self.file_path):
+            print(file)
 
 
 '''
